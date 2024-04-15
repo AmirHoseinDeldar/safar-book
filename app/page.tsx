1810f141e5444a84afa72a9e77a189132064cb22
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-04-15 05:26:49 +0330

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">سلامممممممممم</h1>
+   <>
+   <Hero/>
+   <Camp/>
+   <Guide/>
+   <Features/>
+   <GetApp/>
+   
+   
+   </>
   );
 }
