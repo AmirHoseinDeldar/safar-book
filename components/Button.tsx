99b1b1de9cc2e4a609e49672823059b9caf0e3b1
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-04-15 05:22:15 +0330

    first commit

---
+++ components/Button.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  varient: string;
+  full?: boolean;
+};
+
+const Button = ({ type, title, icon, varient, full }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${varient} ${
+        full && "w-full"
+      }`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "خانه" },
+  { href: "/", key: "how_hilink_work", label: "سفر بوک چگونه کار میکند؟" },
+  { href: "/", key: "services", label: "سرویس ها" },
+  { href: "/", key: "pricing ", label: "قیمت ها " },
+  { href: "/", key: "contact_us", label: "تماس به ما" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "نقشه های واقعی می توانند آفلاین باشند",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "ما راه حلی را برای شما ارائه می دهیم تا بتوانید از برنامه ما در هنگام صعود استفاده کنید، بله نقشه های آفلاین که می توانید در هر زمانی استفاده کنید هیچ سیگنالی در مکان وجود ندارد",
+  },
+  {
+    title: "یک برنامه ماجراجویی تنظیم کنید",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "با دوستان یک ماجراجویی را برنامه ریزی کنید. در روزهای تعطیل، پیشنهادات جالب زیادی از کتاب سفر وجود دارد. به این ترتیب، دیگر بحثی وجود ندارد",
+  },
+  {
+    title: "فناوری با استفاده از واقعیت افزوده",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "فناوری از واقعیت افزوده به عنوان راهنمای مسیر پیاده روی شما در جنگل تا بالای کوه استفاده می کند. در حال حاضر توسط آخرین فناوری بدون اتصال به اینترنت پشتیبانی می شود",
+  },
+  {
+    title: "بسیاری از مکان های جدید هر ماه",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "هر ماه مکان های جدید زیادی وجود دارد، زیرا ما یک جامعه جهانی از کوهنوردان داریم که بهترین تجربیات خود را در مورد کوهنوردی به اشتراک می گذارند.",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "بیشتر بدانید",
+    links: [
+      "درباره سفر بوک",
+      "بیانیه های مطبوعاتی",
+      "محیط",
+      "شغل ها",
+      "سیاست حفظ حریم خصوصی",
+      "تماس با ما",
+    ],
+  },
+  {
+    title: "جامعه ی ما",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "تماس با ما",
+  links: [
+    { label: "مدیریت", value: "909 230 12 02" },
+    { label: "ایمیل", value: "safarbook@akinthil.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "شبکه ها",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
